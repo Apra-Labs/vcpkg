b69cf2ed44ef086941810290c93ab7b148cfba87
zaki <zakik@apra.in>
2023-03-13 17:40:13 +0530

    need mason and patch

--- ports/openh264/portfile.cmake
+++ ports/openh264/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO Apra-labs/openh264
     REF c7310e16dee24775f8fa4a0a813c72b46879e4fb
     SHA512 d83e6494f791a63bebdc35072a9b8bb2812433d281df018aaf2d5fb2b41f58ad3f9d9bf3cc6c11f7d9c8d70576c0e4ce4de12f2125e14441e3a57c83de8e9d4f
+    PATCHES
+        0001-respect-default-library-option.patch  # https://github.com/cisco/openh264/pull/3351
 )
 
 if((VCPKG_TARGET_ARCHITECTURE STREQUAL "x86" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "x64"))
