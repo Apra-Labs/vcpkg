c234f71a7cca12f2dccf1ff9c53f46b3af62bb9f
zaki <zakik@apra.in>
2023-03-23 17:09:40 +0530

    chnaged the openh264 commit

--- ports/openh264/portfile.cmake
+++ ports/openh264/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Apra-Labs/openh264
-    REF 8c6b907ace59e7fda739bb84eefc41c392e12c22
+    REF 4180b7c3f5a883c4aa6bfc82975995b664aa93dc
     SHA512 b5b51d0d6f8d98f0c52fdceefb41e75cc62db3724fe99b1df459e21f379be61741fcbc04f96aa75554ddf3164917cfd363e383cc3263779fe17e6b5add42eff4
     HEAD_REF ForApraPipes
     PATCHES
