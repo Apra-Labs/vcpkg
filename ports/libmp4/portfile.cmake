5edb0ace82b0c00f0d77007aae477a706c553de6
Vinayak Bhustali <vinayakb@apra.in>
2023-07-03 12:05:20 +0530

    portfile.cmake of libmp4 update for windows build

--- ports/libmp4/portfile.cmake
+++ ports/libmp4/portfile.cmake
@@ -1,23 +1,23 @@
 # portfile.cmake for libmp4
 
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO Apra-Labs/libmp4
-  REF 36448ae0d112f2b2472ac7b058acda98e0e0a390
-  SHA512 a0e0022bb0e60cf800ba9914dc34c048a6f706c536148f6cdfcae65c53016c5ea5296404cdb722a33e1569bbb1b813f1200cd4ef9e5872f8459ceb0ae71d1026
-  HEAD_REF forApraPipes2
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Apra-Labs/libmp4
+    SHA512 79ee5d9f0ef1ceadb2d7b9807a74ed5309b017eb371f7f6a8435b24b7a7282d4d647d8ea4f6d63d7bebb344f7a736302ee1caf9ab5fe35229792c1a9a52bb377
+    REF forApraPipes
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
+    PREFER_NINJA 
 )
 
 vcpkg_build_cmake()
 
 vcpkg_install_cmake()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share")
 
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
