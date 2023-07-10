1a5186afe403979171aae195c84111472ecee4b5
zaki <zakik@apra.in>
2023-07-10 16:31:45 +0530

    removed patch

--- ports/openh264/portfile.cmake
+++ ports/openh264/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_from_github(
     REF 48f8b7b416f73801aaf8bc972f3630a5340d3868
     SHA512 d94c0d951d1fe1f6e3875d1d5056e9432e8befaed11844f88c373bace7f865e8be0c6c0e0272ca08aa5535fb94db3939642c5355201e13fc24e39a0b541faa11
     HEAD_REF ApraPipesMaster
-    PATCHES
-        0001-respect-default-library-option.patch  # https://github.com/cisco/openh264/pull/3351
 )
 
 if((VCPKG_TARGET_ARCHITECTURE STREQUAL "x86" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "x64"))
