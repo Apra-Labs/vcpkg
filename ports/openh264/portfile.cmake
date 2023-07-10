2273e4988ef889e3383db7b686d6b9980a7b3e7d
zaki <zakik@apra.in>
2023-07-10 16:26:39 +0530

    updated sha512 value

--- ports/openh264/portfile.cmake
+++ ports/openh264/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Apra-labs/openh264
     REF 48f8b7b416f73801aaf8bc972f3630a5340d3868
-    SHA512 f33c1e01f1d2ff04dcf16e563eacec0bf43235db8afc241ff63c99e5896a2c97efe47d5eeee225bcfc8c49a214d22c42aea640e4d1626b5cc116cc26d59a201d
+    SHA512 d94c0d951d1fe1f6e3875d1d5056e9432e8befaed11844f88c373bace7f865e8be0c6c0e0272ca08aa5535fb94db3939642c5355201e13fc24e39a0b541faa11
     HEAD_REF ApraPipesMaster
     PATCHES
         0001-respect-default-library-option.patch  # https://github.com/cisco/openh264/pull/3351
