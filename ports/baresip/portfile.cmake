b0361c3a59c7132ff7e2c2b21083626be93af0aa
Vinayak Bhustali <vinayakb@apra.in>
2023-07-11 17:01:44 +0530

    baresip portfile commit update

--- ports/baresip/portfile.cmake
+++ ports/baresip/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Apra-Labs/baresip
-  REF c4614cfb66ae2e5aa4f4a66fef0d12e191ea515b
+  REF ea7840ff25a610e2968fc253aed1d774b7073cf9
   SHA512 f0e137bd2f313fa7b1eacd58fdc13c26daa7b212d29afaab4c5641ede5d707eb96fa0f6f38d877d36d6fcaffac81fcae47bad842ce86b6b18546c3431a8d5274
   HEAD_REF forApraPipes
 )
@@ -19,6 +19,8 @@ vcpkg_install_cmake()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
