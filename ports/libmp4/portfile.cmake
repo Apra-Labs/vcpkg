acd41c3c032a69d669eb4294f581e04751489416
mohammedzakikochargi <zakik@apra.in>
2024-05-29 19:17:12 +0530

    Updated vcpkg commit

--- ports/libmp4/portfile.cmake
+++ ports/libmp4/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Apra-Labs/libmp4
-    REF 67627b373753f3075b401362bc858f4acb6e09ea
+    REF caaf26d4ed4f7d731fb0e65fbba2ea9b250d75d1
     SHA512 f6026013e66190edab1ec38bc03c8120c3f4060ad8001ca984b0a2182de11c33bcaff086e3b7bfa0626f081778a7a030eb4152be57694f8949e59f6f79cd07be
     HEAD_REF forApraPipes
 )
